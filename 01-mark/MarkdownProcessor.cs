cdf8361bff0b3e1c4d6843e6ae0eee4b87913da8
Jabrobaka <alexander.kulikov.ru@gmail.com>
2014-11-03 17:40:21 +0500

    Второй тест стал зеленым

--- 01-mark/MarkdownProcessor.cs
+++ 01-mark/MarkdownProcessor.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace _01_mark
@@ -12,17 +13,33 @@ namespace _01_mark
 
         public string ReplaceMarkdownWithHtml(string text)
         {
-            if (string.IsNullOrEmpty(text)) return string.Empty;              
-            var textSplittedByParagraphs = GetParagraphs(text);
-            return textSplittedByParagraphs;
+            if (string.IsNullOrEmpty(text)) return string.Empty;    
+          
+            var textWithParagraphs = SetParagraphs(text);
+            var withProcessedUnderscores = ProcessUnderscores(text);
+            return withProcessedUnderscores;
         }
 
-        private string GetParagraphs(string text)
+        private string SetParagraphs(string text)
         {
             var paragraphs = Regex.Split(text, @"\n\s*\n")
                 .Where(s => !string.IsNullOrEmpty(s))
                 .Select(s => string.Format("<p>{0}</p>", s));
             return string.Join(string.Empty, paragraphs);
+        }
+
+        private string ProcessUnderscores(string text)
+        {
+            var replacedStrings = Regex.Split(text, @"(_([\W\w]+)_)")
+                .Select(ReplaceUnderscoreWithTag);
+            return string.Join(string.Empty, replacedStrings);
+        }
+
+        private string ReplaceUnderscoreWithTag(string toReplace)
+        {
+            var removedUnderScores = toReplace.Substring(1, toReplace.Length - 2);
+            var replacedWithTags = string.Format("<em>{0}</em>", removedUnderScores);
+            return replacedWithTags;
         }
     }
 }
