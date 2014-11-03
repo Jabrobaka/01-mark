58233ebee7f5a479a2f4d0ff64d73f794828808a
Jabrobaka <alexander.kulikov.ru@gmail.com>
2014-11-03 17:08:11 +0500

    Рефакторинг тестов

--- Markdown-Tests/Properties/AssemblyInfo.cs
+++ Markdown-Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Markdown-Markdown_parser_should")]
+[assembly: AssemblyTitle("Markdown-Markdown_processor_should")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Markdown-Markdown_parser_should")]
+[assembly: AssemblyProduct("Markdown-Markdown_processor_should")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
